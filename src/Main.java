9221a077ce6ca4e02756943186ac024a010dac0c
Nahum Litvin <lnahum@gmail.com>
2015-11-14 09:45:41 +0200

    refactored select to seperate method

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,22 @@ public class Main {
     final static String Password = "root";
     public static void main(String[] args) throws Exception{
         Connection conn = null;
+
+        try {
+            conn = getConnection();
+            SelectWorldCountryAndPrintToScreen(conn);
+
+        } finally {
+
+            if (conn != null) conn.close();
+        }
+    }
+
+    private static void SelectWorldCountryAndPrintToScreen(Connection conn) throws SQLException {
         Statement statement = null;
         ResultSet resultSet = null;
         try {
-            conn = getConnection();
+
             statement = conn.createStatement();
             final String SQLQuery = "SELECT * FROM world.city";
             resultSet = statement.executeQuery(SQLQuery);
@@ -25,7 +37,6 @@ public class Main {
         } finally {
             if (resultSet != null) resultSet.close();
             if (statement != null) statement.close();
-            if (conn != null) conn.close();
         }
     }
 
